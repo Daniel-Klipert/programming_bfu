45a6b0afe2693b9b0fb1fc4e67867abb2949dc2c
Daniel-Klipert <klipertdaniel02@gmail.com>
2025-11-14 11:33:22 +0200

    Fix lab6

--- lab6/main.cpp
+++ lab6/main.cpp
@@ -8,7 +8,7 @@ int c = 0;
 
 int* find_zeroes(int** matr, int M, int N){
 
-    int* zeroes = (int*)malloc(M*N*sizeof(int));
+    int* zeroes = (int*)malloc(M*sizeof(int));
 
     
     for (int m = 0; m < M; ++m){
@@ -16,7 +16,7 @@ int* find_zeroes(int** matr, int M, int N){
             if (matr[m][n] == 0){
                 zeroes[c] = m;
                 ++c; 
-                //break; 
+                break; 
             }
         }
     }
@@ -34,68 +34,74 @@ int main(){
     int M = 2;
     int N = 2;
 
-    int matrix[M][N];
-
+    int** dyn_matrix = (int**)calloc(M, sizeof(int*));
     for (int m = 0; m < M; ++m){
+        dyn_matrix[m] = (int*)calloc(N, sizeof(int));
         for (int n = 0; n < N; ++n){
             int a;
             cin >> a;
-            matrix[m][n] = a;
+            dyn_matrix[m][n] = a;
         }
     }
 
-    int A = matrix[0][0];
-    int B = matrix[0][1];
-    int C = matrix[1][0];
-    int D = matrix[1][1];
+
+    int A = dyn_matrix[0][0];
+    int B = dyn_matrix[0][1];
+    int C = dyn_matrix[1][0];
+    int D = dyn_matrix[1][1];
 
     int new_M = M + A;
     int new_N = N + B;
 
-    int** dyn_matrix = (int**)calloc(new_M, sizeof(int*));
+    dyn_matrix = (int**)realloc(dyn_matrix, new_M);
     for (int m = 0; m < new_M; ++m){
-        dyn_matrix[m] = (int*)calloc(new_N, sizeof(int));
+        dyn_matrix[m] = (int*)realloc(dyn_matrix[m], new_N);
 
         for (int n = 0; n < new_N; ++n){
 
-        if (m >= A && n >= B){
-            dyn_matrix[m][n] = matrix[m - A][n - B];
-        }
-        else{
-            dyn_matrix[m][n] = m*C + n*D;
-        }
+            if (m >= A && n >= B){
+                continue;
+            }
+            else{
+                dyn_matrix[m][n] = m*C + n*D;
+            }
     }
 
     }
 
-
-
-    
-
-    
     int* zeroes = find_zeroes(dyn_matrix, new_M, new_N);
-    int k = 0;
+    
+    int r = 0;
     for (int i = 0; i < new_M; ++i){
         
         for (int j = 0; j < c; ++j){
             if (i == zeroes[j]){
-                free(dyn_matrix[i-k]);
-                ++k;
+                ++r;
+                for (int k = i - r ; k < new_M - r;++k){
+                    free(dyn_matrix[k]);
+                    dyn_matrix[k] = dyn_matrix[k + 1];
+                }
+                free(dyn_matrix[new_M - r]);
+                dyn_matrix = (int**)realloc(dyn_matrix, new_M - r);
+                
                 break;
             }
         }
         
     }
-    new_M -= k;
-for (int m = 0; m < new_M; ++m){
-    cout << endl;
-    for (int n = 0; n < new_N; ++n){
-        cout << dyn_matrix[m][n] << '\t';
+    new_M -= r;
+    for (int m = 0; m < new_M; ++m){
+        cout << endl;
+        for (int n = 0; n < new_N; ++n){
+            cout << dyn_matrix[m][n] << '\t';
+        }
     }
-}
 
-free(zeroes);
-free(dyn_matrix);
-}
+    free(zeroes);
+    for(int m =0; m < new_M; ++m){
+        free(dyn_matrix[m]);
+    }
+    free(dyn_matrix);
 
+}
 
